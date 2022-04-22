e4270369794a921572618c684ee646653d378ff5
Sangu Teli <sangamnatth777@gmail.com>
2022-04-22 02:37:38 -0700

    user details,firebaseauth, dotindicator, cardview, search

--- lib/ui/cart.dart
+++ lib/ui/cart.dart
@@ -1,4 +1,5 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
@@ -10,27 +11,75 @@ class Cart extends StatefulWidget {
 }
 
 class _CartState extends State<Cart> {
+  // @override
+  // Widget build(BuildContext context) {
+  //   return MaterialApp(
+  //     title: 'Welcome to Flutter',
+  //     home: Scaffold(
+  //       appBar: AppBar(
+  //         title: const Text('Welcome to Flutter'),
+  //       ),
+  //       body: const Center(
+  //         child: Text('Profile'),
+  //       ),
+  //     ),
+  //   );
+  // }
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
-        ),
-        body: const Center(
-          child: Text('Profile'),
-        ),
+    return Scaffold(
+      body: SafeArea(
+        child: fetchData("users-cart-items"),
       ),
     );
   }
 
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: SafeArea(
-  //       child: fetchData("users-cart-items"),
-  //     ),
-  //   );
-  // }
+  Widget fetchData(String collectionName) {
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection(collectionName)
+          .doc(FirebaseAuth.instance.currentUser!.email)
+          .collection("items")
+          .snapshots(),
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("Something is wrong"),
+          );
+        }
+
+        return ListView.builder(
+            itemCount: snapshot.data == null ? 0 : snapshot.data!.docs.length,
+            itemBuilder: (_, index) {
+              DocumentSnapshot _documentSnapshot = snapshot.data!.docs[index];
+
+              return Card(
+                elevation: 5,
+                child: ListTile(
+                  leading: Text(_documentSnapshot['name']),
+                  title: Text(
+                    "\$ ${_documentSnapshot['price']}",
+                    style: TextStyle(
+                        fontWeight: FontWeight.bold, color: Colors.red),
+                  ),
+                  trailing: GestureDetector(
+                    child: CircleAvatar(
+                      child: Icon(Icons.remove_circle),
+                    ),
+                    onTap: () {
+                      FirebaseFirestore.instance
+                          .collection(collectionName)
+                          .doc(FirebaseAuth.instance.currentUser!.email)
+                          .collection("items")
+                          .doc(_documentSnapshot.id)
+                          .delete();
+                    },
+                  ),
+                ),
+              );
+            });
+      },
+    );
+  }
 }
--- lib/ui/profile.dart
+++ lib/ui/profile.dart
@@ -1,4 +1,5 @@
 // import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
@@ -12,66 +13,72 @@ class _ProfileState extends State<Profile> {
   TextEditingController? _phoneController;
   TextEditingController? _ageController;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Welcome to Flutter',
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('Welcome to Flutter'),
+  // @override
+  // Widget build(BuildContext context) {
+  // //   return MaterialApp(
+  // //     title: 'Welcome to Flutter',
+  // //     home: Scaffold(
+  // //       appBar: AppBar(
+  // //         title: const Text('Welcome to Flutter'),
+  // //       ),
+  // //       body: const Center(
+  // //         child: Text('Profile'),
+  // //       ),
+  // //     ),
+  // //   );
+  // // }
+
+  setDataToTextField(data) {
+    return Column(
+      children: [
+        TextFormField(
+          controller: _nameController =
+              TextEditingController(text: data['name']),
         ),
-        body: const Center(
-          child: Text('Profile'),
+        TextFormField(
+          controller: _phoneController =
+              TextEditingController(text: data['phone']),
         ),
-      ),
+        TextFormField(
+          controller: _ageController = TextEditingController(text: data['age']),
+        ),
+        ElevatedButton(onPressed: () => updateData(), child: Text("Update"))
+      ],
     );
   }
 
-  // setDataToTextField(data){
-  //   return  Column(
-  //     children: [
-  //       TextFormField(
-  //         controller: _nameController = TextEditingController(text: data['name']),
-  //       ),
-  //       TextFormField(
-  //         controller: _phoneController = TextEditingController(text: data['phone']),
-  //       ),
-  //       TextFormField(
-  //         controller: _ageController = TextEditingController(text: data['age']),
-  //       ),
-  //       ElevatedButton(onPressed: ()=>updateData(), child: Text("Update"))
-  //     ],
-  //   );
-  // }
+  updateData() {
+    CollectionReference _collectionRef =
+        FirebaseFirestore.instance.collection("users-form-data");
+    return _collectionRef.doc(FirebaseAuth.instance.currentUser!.email).update({
+      "name": _nameController!.text,
+      "phone": _phoneController!.text,
+      "age": _ageController!.text,
+    }).then((value) => print("Updated Successfully"));
+  }
 
-  // updateData(){
-  //   CollectionReference _collectionRef = FirebaseFirestore.instance.collection("users-form-data");
-  //   return _collectionRef.doc(FirebaseAuth.instance.currentUser!.email).update(
-  //       {
-  //         "name":_nameController!.text,
-  //         "phone":_phoneController!.text,
-  //         "age":_ageController!.text,
-  //       }
-  //       ).then((value) => print("Updated Successfully"));
-  // }
-
-  // @override
-  // Widget build(BuildContext context) {
-  //   return Scaffold(
-  //     body: SafeArea(child: Padding(
-  //       padding: const EdgeInsets.all(20.0),
-  //       child: StreamBuilder(
-  //         stream: FirebaseFirestore.instance.collection("users-form-data").doc(FirebaseAuth.instance.currentUser!.email).snapshots(),
-  //         builder: (BuildContext context, AsyncSnapshot snapshot){
-  //           var data = snapshot.data;
-  //           if(data==null){
-  //             return Center(child: CircularProgressIndicator(),);
-  //           }
-  //           return setDataToTextField(data);
-  //         },
-
-  //       ),
-  //     )),
-  //   );
-  // }
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+          child: Padding(
+        padding: const EdgeInsets.all(20),
+        child: StreamBuilder(
+          stream: FirebaseFirestore.instance
+              .collection("users-form-data")
+              .doc(FirebaseAuth.instance.currentUser!.email)
+              .snapshots(),
+          builder: (BuildContext context, AsyncSnapshot snapshot) {
+            var data = snapshot.data;
+            if (data == null) {
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            return setDataToTextField(data);
+          },
+        ),
+      )),
+    );
+  }
 }
